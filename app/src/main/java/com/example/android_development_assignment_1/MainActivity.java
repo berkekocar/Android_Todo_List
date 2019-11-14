7528947bafac7736472781da211b17517a0af6b7
berkekocar <e.berkekocar@gmail.com>
2019-11-14 21:45:40 +0000

    To-Do List Assignment 1 First Commit.

--- app/src/main/java/com/example/android_development_assignment_1/MainActivity.java
+++ app/src/main/java/com/example/android_development_assignment_1/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.android_development_assignment_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private DBHelper db;
+    private SQLiteDatabase sdb;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        db = new DBHelper(this, "todo_list", null, 1);
+        sdb = db.getWritableDatabase();
     }
 }
