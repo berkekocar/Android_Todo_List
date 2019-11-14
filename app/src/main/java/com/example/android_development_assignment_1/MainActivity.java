f57aacdc43cf76392f524d2b20f630a62ebfbc95
berkekocar <e.berkekocar@gmail.com>
2019-11-14 20:17:33 +0000

    To-Do List Assignment 1 First Commit.

--- app/src/main/java/com/example/android_development_assignment_1/MainActivity.java
+++ app/src/main/java/com/example/android_development_assignment_1/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.android_development_assignment_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
